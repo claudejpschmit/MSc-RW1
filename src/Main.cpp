fb337c3a0b1cb70bbccbb1b25e29406d2eda6f36
Claude Schmit <claudejpschmit@gmailcom>
2014-06-06 11:28:10 +0100

    added proteins

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,20 +12,30 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     /** Makes sure that the parameters are entered correctly when run */
-    if (argc < 2) {
+    if (argc < 3) {
         cout << "This program should be run as: "
-            << argv[0] << " number_of_walks" << endl;
+            << argv[0] << " number_of_walks" << " number_of_proteins" << endl;
         return -1;
     }
 
-    int NoWalks;
+    int NoWalks, NoProteins;
     istringstream (argv[1]) >> NoWalks;
+    istringstream (argv[2]) >> NoProteins;
 
     if (NoWalks < 1) {
-        cout << "number of walks should be positive" << endl;
+        cout << "number of walks should be > 1" << endl;
         return -1;
     }
-    
+    if (NoProteins < 0) {
+        cout << "number of proteins should be positive" << endl;
+        return -1;
+    }
+    if (NoWalks < NoProteins) {
+        cout << "number of proteins should be smaller than the number of steps" << endl;
+        return -1;
+    }
+
+   
     //creating a single n-step random walk
     cout << "Creating a single n-step random walk ..." << endl;
     position current(0,0,0);
@@ -47,9 +57,25 @@ int main(int argc, char *argv[]) {
 
     randomWalk.close();
 
-    // adding portein to the random walk
+    // adding protein to the random walk
     cout << "adding protein to the random walk ..." << endl;
 
+    vector<position> proteins;
+
+    for (int i = 1; i <= NoProteins; ++i)
+    {
+        // -1 to ensure that we don't have a protein of the endpoint.
+        // proteins are uniformly distributed.
+        proteins.push_back(randomWalkStored[i * ((NoWalks - 1)/NoProteins)]);
+    }
+
+    ofstream proteinPositions("proteins.txt");
+    for (int i = 0; i < NoProteins; ++i)
+    {
+        proteinPositions << proteins[i].x << " " << proteins[i].y << " " << proteins[i].z << endl;
+    }
+
+    proteinPositions.close();
     // creating End to end distance data
 
     cout << "Creating end to end distance data ..." << endl;
